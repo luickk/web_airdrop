3956f99a3a17105462138966cc2cc1ecbe442bf9
MrGrimod <gelberdev@gmail.com>
2019-02-06 18:15:43 +0000

    adding cc_state page

--- js-dash/states.js
+++ js-dash/states.js
@@ -1,12 +1,12 @@
 // Subscribing to gps publ
 // ----------------------
 
-var listener = new ROSLIB.Topic({
+var states_listener = new ROSLIB.Topic({
   ros : ros,
   name : '/drone_state',
   messageType : 'cc_node/drone_states'
 });
-listener.subscribe(function(data) {
+states_listener.subscribe(function(data) {
   $("#airborne_state").text(data.airborne);
   $("#in_mission_state").text(data.in_mission);
   $("#landing_phase_state").text(data.landing_phase);
